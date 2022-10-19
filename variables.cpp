51c2d47629e165b98827758e97705faa1272f9b3
Arpit Jain <115658271+iamajarpit@users.noreply.github.com>
2022-10-19 19:53:26 +0530

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void sum(){
+  int glo=5;
+  cout<<glo;
+}
+
+int main() {
+  int glo =8;
+  glo=8423;
+  float pi=3.14;
+    int a=1,b=2;
+    sum();
+    // cout<<"the value of a  "<<a<<endl<<"the value of b  "<<b<<endl<<pi<<endl<<glo<<endl<<sum;
+  cout<<glo;
+
+  
+}
